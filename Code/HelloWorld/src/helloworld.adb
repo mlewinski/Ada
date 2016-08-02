5c734540cf6a4e8dea6198b6ae1f1586c53ca340
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-02 22:26:58 +0200

    Reimplemented HelloWorld using renames

--- Code/HelloWorld/src/helloworld.adb
+++ Code/HelloWorld/src/helloworld.adb
@@ -1,9 +1,10 @@
 with Ada.Text_IO;
 
 procedure HelloWorld is
+   package IO renames Ada.Text_IO;
 begin
-   Ada.Text_IO.Put_Line("Hello, world!");
-   Ada.Text_IO.Put_Line("Test");
-   Ada.Text_IO.Put("Test");
-   Ada.Text_IO.Put_Line("Test");
+   IO.Put_Line("Hello, world!");
+   IO.Put_Line("Test");
+   IO.Put("Test");
+   IO.Put_Line("Test");
 end HelloWorld;
