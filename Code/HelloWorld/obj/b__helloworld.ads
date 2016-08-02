3213ffd1456f234d6dff01e3aaf93e1f0fafcd01
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-02 22:36:32 +0200

    Reimplemented HelloWorld using 'use' keyword

--- Code/HelloWorld/obj/b__helloworld.ads
+++ Code/HelloWorld/obj/b__helloworld.ads
@@ -35,7 +35,7 @@ package ada_main is
    pragma Export (C, main, "main");
 
    type Version_32 is mod 2 ** 32;
-   u00001 : constant Version_32 := 16#6beae788#;
+   u00001 : constant Version_32 := 16#43d43149#;
    pragma Export (C, u00001, "helloworldB");
    u00002 : constant Version_32 := 16#b6df930e#;
    pragma Export (C, u00002, "system__standard_libraryB");
--- Code/HelloWorld/src/helloworld.adb
+++ Code/HelloWorld/src/helloworld.adb
@@ -1,10 +1,11 @@
 with Ada.Text_IO;
+use Ada.Text_IO;
 
 procedure HelloWorld is
-   package IO renames Ada.Text_IO;
+   --package IO renames Ada.Text_IO;
 begin
-   IO.Put_Line("Hello, world!");
-   IO.Put_Line("Test");
-   IO.Put("Test");
-   IO.Put_Line("Test");
+   Put_Line("Hello, world!");
+   --IO.Put_Line("Test");
+   Put("Test");
+   Put_Line("Test");
 end HelloWorld;
